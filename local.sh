29fba0b19d058380357493c4fb61c38c6d085268
Anders Pearson <anders@thraxil.org>
2018-01-03 22:24:26 +0000

    pull in credentials

--- local.sh
+++ local.sh
@@ -5,6 +5,8 @@
 export LUMEN_PORT=9989
 export LUMEN_SECRET=notreallysecret
 export LUMEN_S3_BUCKET=thraxil-lumen-dev
+export LUMEN_S3_ACCESS_KEY=$AWS_ACCESS_KEY
+export LUMEN_S3_SECRET_KEY=$AWS_SECRET_KEY
 
 go build .
 ./lumen
